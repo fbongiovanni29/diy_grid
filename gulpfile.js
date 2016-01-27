654ea802cfe41f93bf86d193f94f3190bd162392
Francis Bongiovanni <fbongiovanni29@gmail.com>
2016-01-26 23:23:54 -0500

    gulp working

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var watch = require("gulp-watch");
+var webserver = require('gulp-webserver')
+//var browserSync = require("browser-sync");
+var reload = browserSync.reload;
+
+gulp.task('webserver', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+    }));
+});
+
+gulp.task('sass', function(){
+  gulp.src('sass/main.scss')
+  .pipe(sass({
+    includePaths: ['sass']
+  }))
+  .pipe(gulp.dest("."))
+});
+
+//pair programming
+/*gulp.task('serve', function(){
+  browserSync.init(['main.css', '*.html'], {
+    server: {
+      baseDir: './',
+    },
+    **The Hub's security workaround**
+    //tunnel: "francis"
+  });
+});*/
+
+gulp.task('watch', ['sass', 'webserver'], function(){
+  gulp.watch(['sass/*.scss', '*.html'], ['sass']);
+})
